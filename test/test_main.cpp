df30fb4dbcbc3bc7341ce58b1da1969b0193fb12
Matthias Hölzl <tc@xantira.com>
2021-12-15 17:19:19 +0100

    Move lib and cli into separate folders

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,10 +1,4 @@
 // Copyright (c) 2021 Dr. Matthias Hölzl.
 
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "OCUnusedMacroInspection"
-// Copyright (c) 2021 Dr. Matthias Hölzl.
-
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.hpp"
-
-#pragma clang diagnostic pop
